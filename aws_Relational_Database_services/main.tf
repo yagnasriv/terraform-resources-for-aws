9a10be23dbed5d5a836c7665c879d2fa6f6a4863
yv@aws <shreeshree3@gmail.com>
2023-10-25 15:59:15 -0500

    added Example: Enabling Automated Backups for an Amazon RDS Database

--- aws_Relational_Database_services/main.tf
+++ aws_Relational_Database_services/main.tf
@@ -4,14 +4,38 @@
 ## and Amazon Aurora, which is a MySQL- and PostgreSQL-compatible database engine built by AWS
 
 
-resource "aws_db_instance" "default" {
+# resource "aws_db_instance" "default" {
+#   allocated_storage    = 10
+#   db_name              = "mysqldbyv"
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t3.micro"
+#   username             = "yagna"
+#   password             = "password"
+#   parameter_group_name = "default.mysql5.7"
+#   skip_final_snapshot  = true
+# }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Example: Enabling Automated Backups for an Amazon RDS Database
+
+resource "aws_db_instance" "example" {
   allocated_storage    = 10
-  db_name              = "mysqldbyv"
+  storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
+  instance_class       = "db.t2.micro"
+  db_name              = "mydatabaseyv"
   username             = "yagna"
   password             = "password"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+  publicly_accessible  = false
+
+  # Enable automated backups
+  backup_retention_period = 1  # Retain backups for 1 days
+  backup_window           = "03:00-04:00"  # Backup window
 }
