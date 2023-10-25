7c37e2cbc9d2e477a976e178042a9a8de6e1a8e3
yv@aws <shreeshree3@gmail.com>
2023-10-25 13:48:08 -0500

    added TF resource for RDS - sql db

---
+++ aws_Relational_Database_services/main.tf
@@ -0,0 +1,17 @@
+## Amazon RDS, or Amazon Relational Database Service, is a managed database service provided by Amazon Web Services (AWS)
+## It is designed to make it easier to set up, operate, and scale a relational database in the cloud
+## AWS RDS supports various database engines, including MySQL, PostgreSQL, Oracle, SQL Server, 
+## and Amazon Aurora, which is a MySQL- and PostgreSQL-compatible database engine built by AWS
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "yagna"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
