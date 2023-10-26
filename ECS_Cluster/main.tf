5d993999d96d15c71b74e0ca1574ca068b57d0b1
yv@aws <shreeshree3@gmail.com>
2023-10-26 12:13:13 -0500

    added tf resource for creating ECS cluster

---
+++ ECS_Cluster/main.tf
@@ -0,0 +1,11 @@
+### To manage AWS Elastic Container Service (ECS) using Terraform, you need to define resources 
+### such as the ECS cluster, task definitions, services, and optionally, networking configurations. 
+### Below is a simplified example of Terraform scripts for managing AWS ECS. 
+### This example includes the creation of an ECS cluster, a task definition, and a service.
+
+### Example for creating ECS Cluster
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-ecs-cluster-yv"
+}
+
