e2608f400e13ac9cd5fd8b3659bcc2d3067cd955
yagnasriv <shreeshree3@gmail.com>
2023-11-06 13:46:37 -0600

    added few tf resources for creating aws lambda function

--- AMAZON_DynamoDB/main.tf
+++ AMAZON_DynamoDB/main.tf
@@ -11,66 +11,65 @@ provider "aws" {
   region = "us-east-1"  # Set your desired AWS region
 }
 
-### Terraform Resource for creating DynamoDB
 
-resource "aws_dynamodb_table" "example" {
-  name           = "ExampleTable"
-  billing_mode   = "PAY_PER_REQUEST"  # Use "PROVISIONED" for provisioned throughput
-  hash_key       = "id"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
+resource "aws_iam_role" "lambda_role" {
+name   = "Spacelift_Test_Lambda_Function_Role"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+ 
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "${path.module}/python/"
+output_path = "${path.module}/python/hello-python.zip"
+}
+ 
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "${path.module}/python/hello-python.zip"
+function_name                  = "Spacelift_Test_Lambda_Function-yv"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
-
-#### Terraform Resource for creating AWS Lambda
-
-resource "aws_lambda_function" "example" {
-  function_name = "ExampleFunction"
-  filename      = "lambda_function_payload.zip"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  role          = aws_iam_role.example.arn
-}
-
-
-### Terraform resource for creating an IAM role
-
-resource "aws_iam_role" "example" {
-  name = "ExampleRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-
-#### Configure the DynamoDB Stream Trigger for Lambda
-
-resource "aws_lambda_event_source_mapping" "example" {
-  event_source_arn = aws_dynamodb_table.example.stream_arn
-  function_name    = aws_lambda_function.example.function_name
-  batch_size       = 5
-}
-
-
-### You'll need to retrieve the DynamoDB Stream ARN from the DynamoDB table to use it as an event source in the Lambda function. 
-### Add the following code to your Terraform configuration:
-
-data "aws_dynamodb_table" "example" {
-  name = aws_dynamodb_table.example.name
-}
-
-output "dynamodb_stream_arn" {
-  value = aws_dynamodb_table.example.stream_arn
-}
---
+++ python/hello-python.zip
@@ -0,0 +1,5 @@
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "${path.module}/python/"
+output_path = "${path.module}/python/hello-python.zip"
+}
