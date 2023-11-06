1528865fe867a0fa459536b93cdaff469f4b352e
yagnasriv <shreeshree3@gmail.com>
2023-11-06 15:33:02 -0600

    Update main.tf

--- AMAZON_DynamoDB/main.tf
+++ AMAZON_DynamoDB/main.tf
@@ -66,7 +66,7 @@ output_path = "${path.module}/python/hello-python.zip"
  
 resource "aws_lambda_function" "terraform_lambda_func" {
 filename                       = "${path.module}/python/hello-python.zip"
-function_name                  = "Spacelift_Test_Lambda_Function-yv"
+function_name                  = "Spacelift_Test_Lambda_Function"
 role                           = aws_iam_role.lambda_role.arn
 handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
