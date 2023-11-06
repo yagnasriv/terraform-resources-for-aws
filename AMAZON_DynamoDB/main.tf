758c64d1332e0e349622630bbf3967242b5eb3a1
yagnasriv <shreeshree3@gmail.com>
2023-11-05 22:53:21 -0600

    updated main.tf

--- AMAZON_DynamoDB/main.tf
+++ AMAZON_DynamoDB/main.tf
@@ -11,19 +11,43 @@ provider "aws" {
   region = "us-east-1"  # Set your desired AWS region
 }
 
+resource "aws_dynamodb_table" "tf_notes_table" { 
+   name = "tf-notes-table-03" 
+   billing_mode = "PROVISIONED" 
+   read_capacity = "30" 
+   write_capacity = "30" 
+   attribute { 
+      name = "noteId" 
+      type = "S" 
+   } 
+   hash_key = "noteId" 
+   ttl { 
+     enabled = true
+     attribute_name = "expiryPeriod"  
+   }
+   point_in_time_recovery { enabled = true } 
+   server_side_encryption { enabled = true } 
+   lifecycle { ignore_changes = [ "write_capacity", "read_capacity" ] }
+} 
 
-resource "aws_dynamodb_table" "tf_notes_table" {
- name = "tf-notes-table-yv02"
- billing_mode = "PROVISIONED"
- read_capacity= "30"
- write_capacity= "30"
- attribute {
-  name = "noteId"
-  type = "S"
- }
- hash_key = "noteId"
- point_in_time_recovery {
-   enabled = true
- }
-
+module  "table_autoscaling" { 
+   source = "snowplow-devops/dynamodb-autoscaling/aws" 
+   table_name = aws_dynamodb_table.tf_notes_table.name
 }
+
+
+# resource "aws_dynamodb_table" "tf_notes_table" {
+#  name = "tf-notes-table-yv02"
+#  billing_mode = "PROVISIONED"
+#  read_capacity= "30"
+#  write_capacity= "30"
+#  attribute {
+#   name = "noteId"
+#   type = "S"
+#  }
+#  hash_key = "noteId"
+#  point_in_time_recovery {
+#    enabled = true
+#  }
+
+# }
